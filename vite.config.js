e88f1ea8f0ba5616d1fe74aac50ba2683026e8df
Mariia Savchenko <mashasavchenko@yahoo.com>
2026-04-17 15:48:50 +0300

    moved public and img folders

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/VITOX-REMBUDGRUP/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
